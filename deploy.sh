1bec167be62f690d4f9ad5fc35629bc99d499269
Hykilpikonna <admin@moecraft.cc>
2020-08-01 17:57:35 -0400

    [+] Separate demo cname record

--- deploy.sh
+++ deploy.sh
@@ -13,13 +13,13 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'vera.hydev.org' > CNAME
+echo 'demo.vera.hydev.org' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:HyDevelop/VeracrossAnalyzer.Client.git master:gh-pages
+git push -f git@github.com:HyDevelop/VeracrossAnalyzerDemo.git master:gh-pages
 
 cd -
